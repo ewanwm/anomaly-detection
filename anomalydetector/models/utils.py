d0447f23d5e4839f34f4a4a51a5178611190315d
Ewan W Miller <e.w.miller@imperial.ac.uk>
2025-10-27 17:27:18 +0000

    add utility function to print summary of model

--- anomalydetector/models/utils.py
+++ anomalydetector/models/utils.py
@@ -37,3 +37,15 @@ def get_latent_dists(model, data_loader, device, quiet=False, num_samples:typing
             llh_dist = np.concatenate((llh_dist, NICE_gaussian_loss(encoded.detach().to(torch.device("cpu")), model.scaling_diag.detach().to(torch.device("cpu")), keepdim=True)), axis=0)
 
     return enc_dist, llh_dist
+
+def summarise_model(model):
+
+    print("##### Model #####'")
+    for module in model.modules():
+        print(module)
+
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+
+    print(f"trainable parameters: {params}")
+    
