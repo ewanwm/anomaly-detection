944cb328b14edcbc1661e72581712d24bdb29a5f
Ewan W Miller <e.w.miller@imperial.ac.uk>
2025-10-27 10:32:41 +0000

    factor model creation and data processing into modules instead of just floating about in notebook

---
+++ anomalydetector/models.py
@@ -0,0 +1,271 @@
+
+import math as m
+
+import normflows as nf
+
+import torch
+from torch import nn
+from torch.distributions.normal import Normal
+from torch.nn import MSELoss
+
+import numpy as np
+
+class VAE(nn.Module):
+    def __init__(self, n_features, latent_size):
+        super().__init__()
+
+        self._n_features = n_features
+        self._latent_size = latent_size
+
+        self.encode = nn.Sequential(
+            nn.Linear(self._n_features, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+        )
+
+        self.f1 = nn.Linear(256, self._latent_size)
+        self.f2 = nn.Linear(256, self._latent_size)
+        
+        self.decode = nn.Sequential(
+            nn.Linear(self._latent_size, 256),
+            nn.ReLU(True),
+            nn.Linear(256, 512),
+            nn.ReLU(True),
+            nn.Linear(512, self._n_features),
+        )
+
+    def forward(self, x):
+        # Encode
+
+        mu = self.f1( self.encode(x) )
+        log_var = self.f2(self.encode(x))
+
+        # Reparametrize variables
+        std = torch.exp(0.5 * log_var)
+        norm_scale = torch.randn_like(std) + 0.00001
+        z_ = mu + norm_scale * std
+
+        # Q0 and prior
+        q0 = Normal(mu, torch.exp((0.5 * log_var)))
+        p = Normal(0.0, 1.0)
+
+        # Decode
+        z_ = z_.view(z_.size(0), self._latent_size)
+        zD = self.decode(z_)
+        out = zD
+
+        return out, mu, log_var
+    
+    def train_batch(self, data):
+
+        rc_batch, mu, log_var = self.forward(data)
+        loss = MSELoss()(data, rc_batch)
+        loss.backward()
+
+        return loss
+    
+class NFVae(nf.NormalizingFlowVAE):
+    
+    def fit_batch(self, data):
+
+        z, log_q, log_p = self(data, self.num_samples)
+        mean_log_q = torch.mean(log_q)
+        mean_log_p = torch.mean(log_p)
+        loss = mean_log_q - mean_log_p
+        loss.backward()
+
+        return loss
+
+
+_get_even = lambda xs: xs[:,0::2]
+_get_odd = lambda xs: xs[:,1::2]
+
+def _interleave(first, second, order):
+    """
+    Given 2 rank-2 tensors with same batch dimension, interleave their columns.
+    
+    The tensors "first" and "second" are assumed to be of shape (B,M) and (B,N)
+    where M = N or N+1, repsectively.
+    """
+    cols = []
+    if order == 'even':
+        for k in range(second.shape[1]):
+            cols.append(first[:,k])
+            cols.append(second[:,k])
+        if first.shape[1] > second.shape[1]:
+            cols.append(first[:,-1])
+    else:
+        for k in range(first.shape[1]):
+            cols.append(second[:,k])
+            cols.append(first[:,k])
+        if second.shape[1] > first.shape[1]:
+            cols.append(second[:,-1])
+    return torch.stack(cols, dim=1)
+
+
+class _BaseCouplingLayer(nn.Module):
+    def __init__(self, dim, partition, nonlinearity):
+        """
+        Base coupling layer that handles the permutation of the inputs and wraps
+        an instance of torch.nn.Module.
+
+        Usage:
+        >> layer = AdditiveCouplingLayer(1000, 'even', nn.Sequential(...))
+        
+        Args:
+        * dim: dimension of the inputs.
+        * partition: str, 'even' or 'odd'. If 'even', the even-valued columns are sent to
+        pass through the activation module.
+        * nonlinearity: an instance of torch.nn.Module.
+        """
+        super(_BaseCouplingLayer, self).__init__()
+        # store input dimension of incoming values:
+        self.dim = dim
+        # store partition choice and make shorthands for 1st and second partitions:
+        assert (partition in ['even', 'odd']), "[_BaseCouplingLayer] Partition type must be `even` or `odd`!"
+
+        self.partition = partition
+        
+        if (partition == 'even'):
+            self._first = _get_even
+            self._second = _get_odd
+        else:
+            self._first = _get_odd
+            self._second = _get_even
+        
+        # store nonlinear function module:
+        # (n.b. this can be a complex instance of torch.nn.Module, for ex. a deep ReLU network)
+        self.add_module('nonlinearity', nonlinearity)
+
+    def forward(self, x):
+        """Map an input through the partition and nonlinearity."""
+        return _interleave(
+            self._first(x),
+            self.coupling_law(self._second(x), self.nonlinearity(self._first(x))),
+            self.partition
+        )
+
+    def inverse(self, y):
+        """Inverse mapping through the layer. Gradients should be turned off for this pass."""
+        return _interleave(
+            self._first(y),
+            self.anticoupling_law(self._second(y), self.nonlinearity(self._first(y))),
+            self.partition
+        )
+
+    def coupling_law(self, a, b):
+        # (a,b) --> g(a,b)
+        raise NotImplementedError("[_BaseCouplingLayer] Don't call abstract base layer!")
+
+    def anticoupling_law(self, a, b):
+        # (a,b) --> g^{-1}(a,b)
+        raise NotImplementedError("[_BaseCouplingLayer] Don't call abstract base layer!")
+
+
+class AdditiveCouplingLayer(_BaseCouplingLayer):
+    """Layer with coupling law g(a;b) := a + b."""
+    def coupling_law(self, a, b):
+        return (a + b)
+    
+    def anticoupling_law(self, a, b):
+        return (a - b)
+
+
+class MultiplicativeCouplingLayer(_BaseCouplingLayer):
+    """Layer with coupling law g(a;b) := a .* b."""
+    def coupling_law(self, a, b):
+        return torch.mul(a,b)
+    
+    def anticoupling_law(self, a, b):
+        return torch.mul(a, torch.reciprocal(b))
+    
+def NICE_gaussian_loss(h, diag, keepdim=False):
+    return torch.sum(diag) - (0.5*torch.sum(torch.pow(h,2),dim=1,keepdim=keepdim) + h.size(1)*0.5*torch.log(torch.tensor(2 * np.pi)))
+
+# wrap above loss functions in Modules:
+class GaussianPriorNICELoss(nn.Module):
+    def __init__(self, size_average=True):
+        super(GaussianPriorNICELoss, self).__init__()
+        self.size_average = size_average
+
+    def forward(self, fx, diag):
+        if self.size_average:
+            return torch.mean(-NICE_gaussian_loss(fx, diag))
+        else:
+            return torch.sum(-NICE_gaussian_loss(fx, diag))
+        
+class NICEModel(nn.Module):
+    def __init__(self, n_features:int, n_flows:int, n_hidden:list[int], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.gauss_loss = GaussianPriorNICELoss()
+
+        coupling_layers = []
+        for i_flow in range(n_flows):
+
+            if i_flow %2 == 0:
+                part = "even"
+            else:
+                part = "odd"
+
+            half_features = -999
+            if part == "even":
+                half_features = m.ceil(n_features / 2)
+            else:
+                half_features = m.floor(n_features / 2)
+
+            # build the list of shift layers 
+            shift_layers = []
+
+            shift_layers.append(nn.Linear(half_features, n_hidden[0]))
+            shift_layers.append(nn.ReLU(True))
+
+            for i_hidden in range(1, len(n_hidden)):
+                shift_layers.append(nn.Linear(n_hidden[i_hidden-1], n_hidden[i_hidden]))
+                shift_layers.append(nn.ReLU(True))
+
+            shift_layers.append( nn.Linear(n_hidden[-1], half_features))
+
+
+            # add the affine layer, made from those shift layers
+            coupling_layers.append(
+                AdditiveCouplingLayer(
+                    dim = n_features,
+                    partition = part,
+                    nonlinearity = nn.Sequential(*shift_layers)
+                )
+            )
+
+
+        self.flows = nn.Sequential(*coupling_layers)
+    
+        self.scaling_diag = nn.Parameter(torch.ones(n_features))
+
+    def forward(self, x):
+        
+        y = self.flows(x)
+
+        return torch.matmul(y, torch.diag(torch.exp(self.scaling_diag)))
+    
+    def inverse(self, y):
+        """Invert a set of draws from gaussians"""
+        with torch.no_grad():
+            x = torch.matmul(y, torch.diag(torch.reciprocal(torch.exp(self.scaling_diag))))
+            
+            for flow in self.flows:
+                x = flow.inverse(x)
+            
+        return x
+    
+    def loss_fn(self, model_pred):
+        return self.gauss_loss(model_pred, self.scaling_diag)
+    
+    def train_batch(self, data):
+
+        enc = self.forward(data)
+        loss = self.loss_fn(enc)
+        loss.backward()
+
+        return loss
+
---
+++ anomalydetector/processing.py
@@ -0,0 +1,151 @@
+
+import os
+import abc
+
+import uproot
+import numpy as np
+import torch
+from torch import masked 
+from torch.utils.data import Dataset
+
+class InMemoryDataset(Dataset, abc.ABC):
+    def __init__(self, filenames:list[str]):
+        super().__init__()
+
+        self._raw_filenames = filenames
+        self._data: torch.Tensor | None = None
+
+
+    def __len__(self):
+        return self._data.shape[0]
+
+
+    def __getitem__(self, index):
+        
+        event = self._data[index]
+        return event, event
+    
+    @classmethod
+    @abc.abstractmethod
+    def process(self):
+        raise NotImplementedError()
+    
+
+
+class InMemoryND280EventDataset(InMemoryDataset):
+
+    def __init__(self, filenames:list[str], branches:list[str], branch_scaling, branch_mask_vals, branch_mask_replace_vals, use_masked_tensor:bool=False, filter:str=None):
+        super().__init__(filenames)
+
+        self._branches = branches
+        self._branch_mask_vals = branch_mask_vals
+        self._branch_scaling = branch_scaling
+        self._branch_mask_replace_vals = torch.tensor(branch_mask_replace_vals)
+        self._processed_filenames = []
+        self._filter = filter
+
+        self.use_masked_tensor = use_masked_tensor
+
+    
+    def get_n_features(self):
+        return len(self._branches)
+
+
+    def process(self):
+
+        file_data_list = []
+
+        # go through each raw file
+        for raw_filename in self._raw_filenames:
+            # open the file
+            with uproot.open(raw_filename) as file:
+
+                # get the sample_sum tree as a pandas array
+                sample_sum = file['sample_sum'].arrays(
+                    filter_branch=lambda b: b.name.find("Graph") == -1 and b.name.find("True") == -1 and b.typename.find("std::vector") == -1,
+                    library='pd',
+                    cut = self._filter
+                )
+
+                file_data:torch.Tensor = torch.tensor(sample_sum[self._branches].values.astype(np.float32))
+                file_data = torch.clip(file_data, min = -1.0, max = 10000)
+                file_data *= self._branch_scaling
+
+                to_mask = sample_sum[self._branches].values.astype(np.float32) == self._branch_mask_vals
+
+                file_data[to_mask] = self._branch_mask_replace_vals.expand(file_data.shape)[to_mask]
+
+                if self.use_masked_tensor:
+                    mask = torch.tensor(sample_sum[self._branches].values.astype(np.float32) != self._branch_mask_vals)
+                    file_data:torch.masked.MaskedTensor = masked.masked_tensor(file_data, mask)
+
+                file_data_list.append(file_data)
+
+        self._data = torch.cat(file_data_list)
+
+
+class nd280EventDataset(Dataset):
+    def __init__(self, root, filenames:list[str], branches:list[str], branch_scaling, branch_mask_vals, branch_mask_replace_vals, use_masked_tensor:bool=False, filter:str=None):
+        super().__init__()
+
+        self.root = root
+        self._raw_filenames = filenames
+        self._branches = branches
+        self._branch_mask_vals = branch_mask_vals
+        self._branch_scaling = branch_scaling
+        self._branch_mask_replace_vals = torch.tensor(branch_mask_replace_vals)
+        self._processed_filenames = []
+        self._filter = filter
+
+        self.use_masked_tensor = use_masked_tensor
+
+    def get_n_features(self):
+        return len(self._branches)
+
+    def process(self):
+
+        event_idx = 0
+
+        # go through each raw file
+        for raw_filename in self._raw_filenames:
+            # open the file
+            with uproot.open(raw_filename) as file:
+
+                # get the sample_sum tree as a pandas array
+                sample_sum = file['sample_sum'].arrays(
+                    filter_branch=lambda b: b.name.find("Graph") == -1 and b.name.find("True") == -1 and b.typename.find("std::vector") == -1,
+                    library='pd',
+                    cut = self._filter
+                )
+
+                for _, row in sample_sum[self._branches].iterrows():
+    
+                    event_tensor:torch.Tensor = torch.tensor(row.values.astype(np.float32))
+                    event_tensor = torch.clip(event_tensor, min = -1.0, max = 5000)
+                    event_tensor *= self._branch_scaling
+
+                    to_mask = row.values.astype(np.float32) == self._branch_mask_vals
+                    event_tensor[to_mask] = self._branch_mask_replace_vals[to_mask]
+
+                    if self.use_masked_tensor:
+                        mask = torch.tensor(row.values.astype(np.float32) != self._branch_mask_vals)
+                        event_tensor:torch.masked.MaskedTensor = masked.masked_tensor(event_tensor, mask)
+
+                    filename = os.path.join(self.root, f'event_{event_idx}_proc.pt')
+                    
+                    self._processed_filenames.append(filename)
+                    torch.save(event_tensor, filename)
+
+                    if event_idx %10000 == 0:
+                        print(f"Processed {event_idx} events")
+
+                    event_idx += 1
+
+    def __len__(self):
+        return len(self._processed_filenames)
+
+
+    def __getitem__(self, index):
+        with torch.serialization.safe_globals([masked.MaskedTensor]):
+            event = torch.load(os.path.join(self.root, f'event_{index}_proc.pt'))
+        return event, event
