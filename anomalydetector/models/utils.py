d5cf1b9956757745de9d77060eb0285429769419
Ewan W Miller <e.w.miller@imperial.ac.uk>
2025-10-29 11:35:26 +0000

    get_latent_dists() now works with any size latent distribution, works with NFVAE and returns decoded distribution

--- anomalydetector/models/utils.py
+++ anomalydetector/models/utils.py
@@ -6,26 +6,29 @@ import numpy as np
 from anomalydetector.models.VAE import NFVAE, VAE
 from anomalydetector.models.NICE import NICEModel, NICE_gaussian_loss
 
-def get_latent_dists(model, data_loader, device, quiet=False, num_samples:typing.Optional[int] = None):
+def get_latent_dists(model, data_loader, device, quiet=False):
     
     n_features = data_loader.dataset.get_n_features()
 
-    enc_dist = np.ndarray((1, n_features))
-    llh_dist = np.ndarray((1, 1))
+    enc_dist_list     = []
+    llh_dist_list     = []
+    decoded_dist_list = []
 
     if isinstance(model, NFVAE):
         for x, n in data_loader:
             x = x.to(device)
-            encoded, log_q, log_p = model(x, num_samples)
+            encoded, log_q, log_p = model(x)
             decoded = model.decoder(encoded)
-            enc_dist = np.concatenate((enc_dist, encoded.detach().to(torch.device("cpu"))), axis=0)
-
+            decoded_dist_list.append(decoded.detach().to(torch.device("cpu"))[:,0,:])
+            enc_dist_list.append(encoded.detach().to(torch.device("cpu"))[:,0,:])
 
     if isinstance(model, VAE):
         for x, n in data_loader:
             x = x.to(device)
             encoded = model.encode(x)
-            enc_dist = np.concatenate((enc_dist, encoded.detach().to(torch.device("cpu"))), axis=0)
+            decoded = model.decode(encoded)
+            decoded_dist_list.append(decoded.detach().to(torch.device("cpu")))
+            enc_dist_list.append(encoded.detach().to(torch.device("cpu")))
 
 
     if isinstance(model, NICEModel):
@@ -33,10 +36,19 @@ def get_latent_dists(model, data_loader, device, quiet=False, num_samples:typing
         for x, n in data_loader:
             x = x.to(device)
             encoded = model(x)
-            enc_dist = np.concatenate((enc_dist, encoded.detach().to(torch.device("cpu"))), axis=0)
-            llh_dist = np.concatenate((llh_dist, NICE_gaussian_loss(encoded.detach().to(torch.device("cpu")), model.scaling_diag.detach().to(torch.device("cpu")), keepdim=True)), axis=0)
+            decoded = model.inverse(encoded)
+            enc_dist_list.append(encoded.detach().to(torch.device("cpu")))
+            decoded_dist_list.append(decoded.detach().to(torch.device("cpu")))
+            llh_dist_list.append(NICE_gaussian_loss(encoded.detach().to(torch.device("cpu")), model.scaling_diag.detach().to(torch.device("cpu")), keepdim=True))
 
-    return enc_dist, llh_dist
+    enc_dist     = np.concatenate(enc_dist_list, axis = 0)
+    decoded_dist = np.concatenate(decoded_dist_list, axis = 0)
+
+    llh_dist = None
+    if llh_dist_list is []:
+        llh_dist_list = np.concatenate(llh_dist_list, axis = 0)
+        
+    return enc_dist, llh_dist, decoded_dist
 
 def summarise_model(model):
 
